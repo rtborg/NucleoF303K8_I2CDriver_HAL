083fdc02e60e6b06aca89264a64bad83f75c89f7
rtborg <b.kereziev@gmail.com>
2019-09-08 11:44:45 +0100

    Timer 3 functions added

--- Inc/main.h
+++ Inc/main.h
@@ -70,6 +70,8 @@ void Error_Handler(void);
 #define VCP_RX_GPIO_Port GPIOA
 #define LD3_Pin GPIO_PIN_3
 #define LD3_GPIO_Port GPIOB
+#define Debug_Pin GPIO_PIN_4
+#define Debug_Pin_Port GPIOB
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
